761fa175c6b9ff93b4c30ca7712e6cbbc0fe2cfd
Fadhil <boggaming098.com@gmail.com>
2024-07-31 19:27:05 +0530

    new

---
+++ app/api/webhook.js
@@ -0,0 +1,25 @@
+// api/webhook/router.js
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const mode = searchParams.get('hub.mode');
+  const challenge = searchParams.get('hub.challenge');
+  const verifyToken = searchParams.get('hub.verify_token');
+
+  // Check the mode and verify token
+  if (mode === 'subscribe' && verifyToken === process.env.VERIFY_TOKEN) {
+    return NextResponse.json(challenge);
+  } else {
+    return new NextResponse('Forbidden', { status: 403 });
+  }
+}
+
+export async function POST(req) {
+  const body = await req.json();
+  console.log('Received webhook event:', body);
+
+  // Process the webhook event here
+
+  return new NextResponse('EVENT_RECEIVED');
+}
