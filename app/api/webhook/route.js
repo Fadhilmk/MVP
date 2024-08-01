32b48d434a43ce34a67e5af118ba2845a5183010
Fadhil <boggaming098.com@gmail.com>
2024-08-01 20:06:38 +0530

    hkk

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,9 +1,9 @@
+// app/api/webhook/route.js
 import { NextResponse } from 'next/server';
 
 const VERIFY_TOKEN = 'sample'; // Your verification token
 
 export async function GET(request) {
-  console.log("hey")
   const url = new URL(request.url);
   const mode = url.searchParams.get('hub.mode');
   const token = url.searchParams.get('hub.verify_token');
@@ -12,16 +12,9 @@ export async function GET(request) {
   if (mode && token) {
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       console.log('Webhook verified!');
-      return NextResponse.json(challenge);
+      return NextResponse.json(challenge, { headers: { 'Content-Type': 'text/plain' } });
     } else {
       return NextResponse.json('Forbidden', { status: 403 });
     }
   }
-}
-
-export async function POST(request) {
-  const data = await request.json();
-  console.log('Received data:', data);
-  // Save or process the data as needed
-  return NextResponse.json('Event received');
 }
