38a54dac8172e0a5036b1f4758565f63b665597c
Fadhil <boggaming098.com@gmail.com>
2024-08-01 19:47:53 +0530

    hellooo

--- app/api/sendMessage/route.js
+++ app/api/sendMessage/route.js
@@ -7,7 +7,7 @@ export async function POST(request) {
   const { to, message } = await request.json();
 
   try {
-    const response = await fetch(`https://graph.facebook.com/v16.0/${PHONE_NUMBER_ID}/messages`, {
+    const response = await fetch(`https://graph.facebook.com/v20.0/${PHONE_NUMBER_ID}/messages`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${ACCESS_TOKEN}`,
